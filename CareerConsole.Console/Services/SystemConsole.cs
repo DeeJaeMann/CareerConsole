7b3ae78072fb56aba57c2271c8f22c1ea806ae18
Dee J. Mann <dee.j.mann@hotmail.com>
2025-05-12 19:33:53 -0400

    Added SystemConsole implementing IConsole  Changes to be committed: 	new file:   Services/SystemConsole.cs

---
+++ CareerConsole.Console/Services/SystemConsole.cs
@@ -0,0 +1,22 @@
+using System;
+using CareerConsole.Core.Interfaces;
+
+namespace CareerConsole.Console.Services
+{
+    public class SystemConsole : IConsole
+    {
+        /* This class implements the IConsole interface and provides
+         * the actual console operations. This allows for easier
+         * testing and mocking of the console operations.
+         */
+        public void WriteLine(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+
+        public string ReadLine()
+        {
+            return System.Console.ReadLine();
+        }
+    }
+}
