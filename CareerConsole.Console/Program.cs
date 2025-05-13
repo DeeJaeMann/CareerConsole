8483c2531e6bd711fa02eab51b214741f9bf33ee
Dee J. Mann <dee.j.mann@hotmail.com>
2025-05-13 08:32:08 -0400

    Refactor and remove test class  Changes to be committed: 	modified:   CareerConsole.Console/Program.cs 	modified:   CareerConsole.Console/UI/Menu.cs 	deleted:    CareerConsole.Core/Interfaces/DummyClass.cs

--- CareerConsole.Console/Program.cs
+++ CareerConsole.Console/Program.cs
@@ -6,10 +6,10 @@ class Program
     static void Main(string[] args)
     {
         // Create an instance of the SystemConsole class
-        var console = new SystemConsole();
+        SystemConsole console = new();
         
         // Create an instance of the Menu class and pass the console instance to it
-        var menu = new Menu(console);
+        Menu menu = new(console);
         // Display the menu
         menu.DisplayMenu();
     }
