00a1e2bc975b706d88a110299e498403978a1c57
Dee J. Mann <dee.j.mann@hotmail.com>
2025-05-13 11:15:45 -0400

    Removed incorrect XML comments  Changes to be committed: 	modified:   CareerConsole.Console/Services/SystemConsole.cs

--- CareerConsole.Console/Services/SystemConsole.cs
+++ CareerConsole.Console/Services/SystemConsole.cs
@@ -9,27 +9,15 @@ namespace CareerConsole.Console.Services
          /// </summary>
         public void WriteLine(string message)
         {
-            /// <summary>
-            /// Writes a message to the console with a new line.
-            /// </summary>
-            /// <param name="message">The message to write.</param>
             System.Console.WriteLine(message);
         }
 
         public void Write(string message)
         {
-            /// <summary>
-            /// Writes a message to the console without a new line.
-            /// </summary>
-            /// <param name="message">The message to write.</param>
             System.Console.Write(message);
         }
         public string ReadLine()
         {
-            /// <summary>
-            /// Reads a line of input from the console.
-            /// </summary>
-            /// <returns>The input string.</returns>
             return System.Console.ReadLine()!;
         }
 
